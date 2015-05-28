6e0b0db0946166e1fcadeb817e172cb32e71ffb4
Guillermo A. Perez <gperezme@ulb.ac.be>
2015-05-28 16:29:15 +0200

    scripts ready

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 DIR=`dirname $0`/
 cd ${DIR}source
 tar -zxvf cudd-2.5.1.tar.gz
-cd ${DIR}source/cudd-2.5.1
+cd cudd-2.5.1
 make objlib
-cd ${DIR}source
+cd ..
 make
-cd ${DIR}
+cd ..
 mkdir ${DIR}binary
 cp ${DIR}source/abssynthe ${DIR}binary/abssynthe
