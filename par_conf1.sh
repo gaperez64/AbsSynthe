24c25a462ddf5ee1a328212f453db3e780356ae0
Guillermo A. Perez <gperezme@ulb.ac.be>
2015-05-28 16:39:34 +0200

    all scripts ready

--- par_conf1.sh
+++ par_conf1.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 DIR=`dirname $0`/
 # $1 contains the input filename (the name of the AIGER-file).
-# $2 (if present) contains the output filename (your synthesis result, also in
-#    AIGER format).
-if [[ $# < 2 ]]; then
-    COMMAND="${DIR}binary/abssynthe -p $1"
-    $COMMAND
-    if [[ $res == 10 ]]; then
-        echo "REALIZABLE"
-    else
-        echo "UNREALIZABLE"
-    fi
+COMMAND="${DIR}binary/abssynthe -p $1"
+$COMMAND
+res=$?
+if [[ $res == 10 ]]; then
+    echo "REALIZABLE"
 else
-    COMMAND="${DIR}binary/abssynthe -p $1 -o $2"
-    $COMMAND
-    res=$?
-    if [[ $res == 10 ]]; then
-        cat ${DIR}$2
-    else
-        echo "UNREALIZABLE"
-    fi
+    echo "UNREALIZABLE"
 fi
 exit $res
--- seq_synth_conf1.sh
+++ seq_synth_conf1.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 DIR=`dirname $0`/
 # $1 contains the input filename (the name of the AIGER-file).
-COMMAND="${DIR}binary/abssynthe -c 2 $1"
+COMMAND="${DIR}binary/abssynthe $1 -o $1-result.aag"
 $COMMAND
 res=$?
 if [[ $res == 10 ]]; then
-    echo "REALIZABLE"
+    cat "$1-result.aag"
 else
     echo "UNREALIZABLE"
 fi
