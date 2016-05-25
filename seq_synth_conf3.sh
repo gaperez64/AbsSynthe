1d2189b55dec0396abca9e50e183b9cea82c6484
Guillermo A. Perez <guillermo@lit9.ulb.ac.be>
2016-05-25 15:22:46 +0200

    updated configurations to add winning region in synthesis ones

--- seq_synth_conf3.sh
+++ seq_synth_conf3.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 DIR=`dirname $0`/
 # $1 contains the input filename (the name of the AIGER-file).
-COMMAND="${DIR}binary/abssynthe -a2048 -c 1 $1 -o $1-result.aag"
+COMMAND="${DIR}binary/abssynthe -a2048 -c 1 $1 -o $1-result.aag -w $1-wregion.aag"
 $COMMAND
 res=$?
 if [[ $res == 10 ]]; then
+    echo "REALIZABLE"
     cat "$1-result.aag"
+    echo "WINNING_REGION"
+    cat "$1-wregion.aag"
 else
     echo "UNREALIZABLE"
 fi
